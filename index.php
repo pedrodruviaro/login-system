422304fee7d26f7ed94e81d0a8ca49cb51eaf05b
pedrodruviaro <pedrodruviaro@gmail.com>
2024-11-18 13:49:51 -0300

    feat: setup

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <h2>Login</h2>
+    <form action="includes/login.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <button type="submit">Login</button>
+    </form>
+
+    <br>
+    <br>
+
+    <h2>Signup</h2>
+    <form action="includes/login.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <input type="email" name="email" placeholder="Email">
+        <button type="submit">Register</button>
+    </form>
+</body>
+</html>
